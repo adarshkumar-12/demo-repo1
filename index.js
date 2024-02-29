3a97b44fe3e5dd7db7b91b528d48ef4fe8cd8b8a
adarshkumar-12 <adarshkumar3775@gmail.com>
2024-02-29 23:29:13 +0530

    Added some lines in index.js

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ console.log("How Are You?");
 console.log("Hey")
 console.log("Hello Universe")
 console.log("Just to let you know i am fine");
+console.log("Adding something")
+console.log("Adding something 1")
+console.log("Adding something 2")
